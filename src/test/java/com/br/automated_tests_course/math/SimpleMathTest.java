f0ce84c8d75fd97f357ca787ce245b5f9fb72f6e
Pedro Pimentel <pedrohenriquepimenteldev05@gmail.com>
2026-01-23 12:53:48 -0300

    Conhecendo as Lazy Assert Messages

--- src/test/java/com/br/automated_tests_course/math/SimpleMathTest.java
+++ src/test/java/com/br/automated_tests_course/math/SimpleMathTest.java
@@ -8,12 +8,19 @@ public class SimpleMathTest {
     @Test
     void testSum(){
         SimpleMath math = new SimpleMath();
-        Double actual = math.sum(6.2D,2D);
+        //Adicionando os números em variáveis para parametrizar a msg.
+        double firstNumber = 6.2D;
+        double secondNumber = 2D;
+        Double actual = math.sum(firstNumber,secondNumber);
         double expected = 8.2D;
 
-        assertEquals(expected,actual,"6.2 + 2 did not produce 8.2!");
-        assertNotEquals(9.2, actual); //Garante que o valor atual não seja igual 9.2
-        assertNotNull(actual); // Se o valor "actual" (Objeto) for nulo, a asserção falha e o teste é interrompido com erro
+        /*
+        * Adicionarei uma lambda function para que o comando só seja executado quando realmente houver erro.
+        * Buscando otimizar a performance do código.*/
+        assertEquals(expected,actual,
+                () -> firstNumber + " + " + secondNumber + " did not produce " + expected + "!");
+        assertNotEquals(9.2, actual);
+        assertNotNull(actual);
 
     }
 }
