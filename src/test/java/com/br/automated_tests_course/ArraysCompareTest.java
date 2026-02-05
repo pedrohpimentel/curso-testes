70e31f88882301e78bb6bad4b11ccb448c773b7c
Pedro Pimentel <pedrohenriquepimenteldev05@gmail.com>
2026-02-05 13:14:42 -0300

    feat: Testando Performance em JUnit Tests

--- src/test/java/com/br/automated_tests_course/ArraysCompareTest.java
+++ src/test/java/com/br/automated_tests_course/ArraysCompareTest.java
@@ -2,22 +2,24 @@ package com.br.automated_tests_course;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ArraysCompareTest {
 
     @Test
-    void test(){
+    // @Timeout(1) -> 1 SEGUNDO PARA EXECUTAR, SE NAO EXECUTAR ESTOURA UM ERRO.
+    @Timeout(value = 15, unit = TimeUnit.MILLISECONDS) //15 MILISSEGUNDOS PARA EXECUTAR, SE NAO EXECUTAR ESTOURA UM ERRO.
+    void testSortPerformance(){
         int[] numbers = {25,8,21,32,3};
-        int[] expectedArray = {3,8,21,25,32};
-
-        Arrays.sort(numbers);
-
-        // para comparar um array com o outros utilize : assertArrayEquals().
-        assertArrayEquals(numbers, expectedArray);
+        for (int i = 0; i < 1000000000  ; i++) {
+            numbers[0] = i;
+            Arrays.sort(numbers);
+        }
 
     }
 }
