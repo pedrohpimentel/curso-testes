c3e5816aaa2dadaf0ad43c33008ebabd3c32ab40
Pedro Pimentel <pedrohenriquepimenteldev05@gmail.com>
2026-02-05 13:49:51 -0300

    feat: @ParameterizedTest com @CsvSource

--- src/test/java/com/br/automated_tests_course/math/SimpleMathTestS4.java
+++ src/test/java/com/br/automated_tests_course/math/SimpleMathTestS4.java
@@ -3,6 +3,7 @@ package com.br.automated_tests_course.math;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -24,10 +25,15 @@ public class SimpleMathTestS4 {
 
     }
 
-    @ParameterizedTest
     @DisplayName("Test 6.2 / 2 = 3.1")
+    @ParameterizedTest
     // @MethodSource("testDivisionInputParameters")
-    @MethodSource
+    //@MethodSource
+    @CsvSource({
+            "6.2,2,3.1",
+            "71,14,5.07",
+            "18.3,3.1,5.90"
+    })
     void testDivision(double firstNumber,  double secondNumber, double expected){
 
         System.out.println("Test " + firstNumber + " / " + secondNumber + " = " + expected);
@@ -38,12 +44,12 @@ public class SimpleMathTestS4 {
     }
 
     // public static Stream<Arguments> testDivisionInputParameters()
-    public static Stream<Arguments> testDivision(){
+    /*public static Stream<Arguments> testDivision(){
         return Stream.of(
                 Arguments.of(6.2D,2D,3.1D),
                 Arguments.of(71D,14D,5.07d),
                 Arguments.of(18.3,3.1D,5.9D)
         );
-    }
+    }*/
 
 }
