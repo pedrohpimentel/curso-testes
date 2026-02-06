e697f353abb654cd4a9424aabb67a690318c5c67
Pedro Pimentel <pedrohenriquepimenteldev05@gmail.com>
2026-02-06 05:43:19 -0300

    feat: @TestMethodOrder Ordernação por Índice com @Order

---
+++ src/test/java/com/br/automated_tests_course/nomath/MethodOrderedByOrder.java
@@ -0,0 +1,29 @@
+package com.br.automated_tests_course.nomath;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class) // Irá ordenar por índice
+public class MethodOrderedByOrder {
+
+    @Test
+    @Order(1) // Setamos a ordem pelo @Order
+    void testB(){
+        System.out.println("Running test B: ");
+    }
+
+    @Test
+    @Order(2)
+    void testC(){
+        System.out.println("Running test C: ");
+    }
+
+    @Test
+    @Order(3)
+    void testA(){
+        System.out.println("Running test A: ");
+    }
+
+}
