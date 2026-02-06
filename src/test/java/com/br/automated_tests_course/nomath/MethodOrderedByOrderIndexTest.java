a0610e3ec8ddfdff85bf8ab63da59f041a59e2b1
Pedro Pimentel <pedrohenriquepimenteldev05@gmail.com>
2026-02-06 06:16:22 -0300

    feat: Alterando o ciclo de vida do JUnit 5 com @TestInstance

--- src/test/java/com/br/automated_tests_course/nomath/MethodOrderedByOrderIndexTest.java
+++ src/test/java/com/br/automated_tests_course/nomath/MethodOrderedByOrderIndexTest.java
@@ -1,30 +1,39 @@
 package com.br.automated_tests_course.nomath;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
-@Order(3)
+//@Order(3)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class) // Irá ordenar por índice
-public class MethodOrderedByOrder {
+public class MethodOrderedByOrderIndexTest {
+
+    StringBuilder actualValue = new StringBuilder("");
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("The actual value is " + actualValue);
+    }
 
     @Test
     @Order(1) // Setamos a ordem pelo @Order
     void testB(){
         System.out.println("Running test B: ");
+        actualValue.append("3");
     }
 
     @Test
     @Order(2)
     void testC(){
         System.out.println("Running test C: ");
+        actualValue.append("1");
     }
 
     @Test
     @Order(3)
     void testA(){
         System.out.println("Running test A: ");
+        actualValue.append("2");
     }
+
 
 }
